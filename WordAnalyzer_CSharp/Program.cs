3f767bc8f4563a09a9e535b63c9f4018efeac691
Danealor <iamblonder@gmail.com>
2016-09-20 17:49:11 -0400

    Add files via upload

---
+++ WordAnalyzer_CSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace WordAnalyzer_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(new string(File.ReadAllText(args[0]).Where(c => char.IsLetter(c) || c == ' ').ToArray()).ToLower().Split(' ').GroupBy(s => s).OrderBy(g => g.Key).SelectMany(g => g.Key + ": " + g.Count() + "\n").ToArray());
+        }
+    }
+}
